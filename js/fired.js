11bec92feb82e0f7381839b5edcfd64c4b227a9b
Lemon <lemon@hirelemon.com>
2015-10-20 14:13:33 -0500

    Super minor tweaks I forgot to commit earlier.

--- js/fired.js
+++ js/fired.js
@@ -45,10 +45,10 @@ function newFire(ago) {
       "Let's charge customers for our service!",
       "I don't even tweet!",
       "Nobody lints MY code!",
-      "But it's my daughter's birthday!"
+      "It's my daughter's birthday!"
     ];
     var shout = shouts[Math.floor(Math.random()*shouts.length)];
-    msg = 'Insiders recognized '+name+'&apos;s voice shouting &ldquo;'+shout+'!&rdquo; '+company+' cannot speak to the matter directly, but they are looking for a new '+job+'.';
+    msg = 'Insiders recognized '+name+'&apos;s voice shouting &ldquo;'+shout+'&rdquo; '+company+' cannot speak to the matter directly, but they are looking for a new '+job+'.';
   } else if (r == 5) {
     var posting = jobPostings[Math.floor(Math.random()*jobPostings.length)];
     msg = company+' is in need of a new '+job+'. Posting says &ldquo;'+posting+'&rdquo; Pay starts at '+salary;
